1b6090c26fa4cba691b24e87f9b9cb5ae66269a1
Etienne LEGRAND <etienne.legrand3@ecoles-epsi.net>
2024-04-15 11:28:17 +0200

    Création du matchmaking

---
+++ app/contexts/socket.context.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Platform } from "react-native";
+import io from "socket.io-client";
+
+console.log("Emulation OS Platform: ", Platform.OS);
+export const socketEndpoint =
+  Platform.OS === "web" ? "http://localhost:3000" : "http://10.60.104.242:3000";
+
+export const socket = io(socketEndpoint, {
+  transports: ["websocket"],
+});
+
+export let hasConnection = false;
+
+socket.on("connect", () => {
+  console.log("connect: ", socket.id);
+  hasConnection = true;
+});
+
+socket.on("disconnect", () => {
+  hasConnection = false;
+  console.log("disconnected from server"); // undefined
+  socket.removeAllListeners();
+});
+
+export const SocketContext = React.createContext();
--- app/screens/home.screen.js
+++ app/screens/home.screen.js
@@ -1,55 +1,20 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Platform } from "react-native";
-import io from "socket.io-client";
+import { StyleSheet, View, Button } from "react-native";
 
-console.log("Emulation OS Platform: ", Platform.OS);
-
-export const socketEndpoint =
-  Platform.OS === "web" ? "http://localhost:3000" : "http://10.60.104.242:3000";
-
-export default function App() {
-  const [hasConnection, setConnection] = useState(false);
-  const [time, setTime] = useState(null);
-
-  useEffect(function didMount() {
-    const socket = io(socketEndpoint, {
-      transports: ["websocket"],
-    });
-
-    socket.io.on("open", () => setConnection(true));
-    socket.io.on("close", () => setConnection(false));
-
-    socket.on("time-msg", (data) => {
-      setTime(new Date(data.time).toString());
-    });
-
-    return function didUnmount() {
-      socket.disconnect();
-      socket.removeAllListeners();
-    };
-  }, []);
-
+export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      {!hasConnection && (
-        <>
-          <Text style={styles.paragraph}>
-            Connecting to {socketEndpoint}...
-          </Text>
-          <Text style={styles.footnote}>
-            Make sure the backend is started and reachable
-          </Text>
-        </>
-      )}
-
-      {hasConnection && (
-        <>
-          <Text style={[styles.paragraph, { fontWeight: "bold" }]}>
-            Server time
-          </Text>
-          <Text style={styles.paragraph}>{time}</Text>
-        </>
-      )}
+      <View>
+        <Button
+          title="Jouer en ligne"
+          onPress={() => navigation.navigate("OnlineGameScreen")}
+        />
+      </View>
+      <View>
+        <Button
+          title="Jouer contre le bot"
+          onPress={() => navigation.navigate("VsBotGameScreen")}
+        />
+      </View>
     </View>
   );
 }
@@ -60,12 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  paragraph: {
-    fontSize: 16,
-  },
-  footnote: {
-    fontSize: 14,
-    fontStyle: "italic",
   },
 });
