238f08754aba403320db172e4bd2915ae36df013
franswap <fplaniol@outlook.fr>
2024-04-24 08:13:39 +0200

    fix grid board component

--- app/components/board/board.component.js
+++ app/components/board/board.component.js
@@ -1,8 +1,9 @@
-import { View, Text, StyleSheet } from "react-native";
-import PlayerTimer from "./timer/player-timer.component";
-import OpponentTimer from "./timer/opponent-timer.component";
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
 import PlayerDeck from "./decks/player-deck.component";
 import OpponentDeck from "./decks/opponent-deck.component";
+import PlayerTimer from "./timers/player-timer.component";
+import OpponentTimer from "./timers/opponent-timer.component";
 import Choices from "./choices/choices.component";
 import Grid from "./grid/grid.component";
 
@@ -31,6 +32,7 @@ const PlayerInfos = () => {
 };
 
 const PlayerScore = () => {
+
   return (
     <View style={styles.playerScoreContainer}>
       <Text>PlayerScore</Text>
@@ -39,32 +41,40 @@ const PlayerScore = () => {
 };
 
 const Board = ({ gameViewState }) => {
+
   return (
+
     <View style={styles.container}>
-      <View style={[styles.row, { height: "5%" }]}>
+
+      <View style={[styles.row, { height: '5%' }]}>
         <OpponentInfos />
         <View style={styles.opponentTimerScoreContainer}>
           <OpponentTimer />
           <OpponentScore />
         </View>
       </View>
-      <View style={[styles.row, { height: "25%" }]}>
+
+      <View style={[styles.row, { height: '25%' }]}>
         <OpponentDeck />
       </View>
-      <View style={[styles.row, { height: "40%" }]}>
+
+      <View style={[styles.row, { height: '40%' }]}>
         <Grid />
         <Choices />
       </View>
-      <View style={[styles.row, { height: "25%" }]}>
+
+      <View style={[styles.row, { height: '25%' }]}>
         <PlayerDeck />
       </View>
-      <View style={[styles.row, { height: "5%" }]}>
+
+      <View style={[styles.row, { height: '5%' }]}>
         <PlayerInfos />
         <View style={styles.playerTimerScoreContainer}>
           <PlayerTimer />
           <PlayerScore />
         </View>
       </View>
+
     </View>
   );
 };
@@ -72,72 +82,96 @@ const Board = ({ gameViewState }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
   },
   row: {
-    flexDirection: "row",
-    width: "100%",
+    flexDirection: 'row',
+    width: '100%',
     borderBottomWidth: 1,
-    borderColor: "black",
+    borderColor: 'black',
   },
   opponentInfosContainer: {
     flex: 7,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRightWidth: 1,
-    borderColor: "black",
-    backgroundColor: "lightgrey",
+    borderColor: 'black',
+    backgroundColor: "lightgrey"
   },
   opponentTimerScoreContainer: {
     flex: 3,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "lightgrey",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
   },
-  gridContainer: {
-    flex: 7,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRightWidth: 1,
-    borderColor: "black",
+  opponentTimerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  choicesContainer: {
-    flex: 3,
-    justifyContent: "center",
-    alignItems: "center",
+  opponentScoreContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  deckPlayerContainer: {
+  deckOpponentContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     borderBottomWidth: 1,
-    borderColor: "black",
+    borderColor: "black"
+  },
+  gridContainer: {
+    flex: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderColor: 'black',
+  },
+  choicesContainer: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
+  },
+  deckPlayerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'black',
   },
   playerInfosContainer: {
     flex: 7,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRightWidth: 1,
-    borderColor: "black",
-    backgroundColor: "lightgrey",
+    borderColor: 'black',
+    backgroundColor: "lightgrey"
   },
   playerTimerScoreContainer: {
     flex: 3,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "lightgrey",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
+  },
+  playerTimerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
   },
   playerScoreContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "lightgrey",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
   },
 });
 
