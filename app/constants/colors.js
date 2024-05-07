506b6836632c91a0691692b3857994235709fdf3
Etienne Legrand <etiennewazer@gmail.com>
2024-05-07 22:56:42 +0200

    Correction de la couleur lightPink

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,9 +1,9 @@
 export const colors = {
-    white: "#FBF5EF",
-    beige: "#F2D3AB",
-    ligthPink: "#C69FA5",
-    mauve: "#8B6D9C",
-    indigo: "#494d7e",
-    darkBlue: "#272744",
-    black: "#000000",
-}
+  white: "#FBF5EF",
+  beige: "#F2D3AB",
+  lightPink: "#C69FA5",
+  mauve: "#8B6D9C",
+  indigo: "#494d7e",
+  darkBlue: "#272744",
+  black: "#000000",
+};
